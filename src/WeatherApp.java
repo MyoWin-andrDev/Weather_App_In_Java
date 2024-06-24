0d1497dbcb9dd4ca833c4cf1a51b0fd957bcc395
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-06-24 18:36:44 +0700

    Initial Commit

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class WeatherApp {
     public static void main(String[] args) {
-        String name ;
+        Scanner scanner = new Scanner(System.in);
+        String city;
+        do{
+            //Retrieve User Input
+            System.out.println("*===============================*");
+            System.out.println("Enter City ( Say 'No' to Exit)");
+            city = scanner.nextLine();
+
+            if(city.equalsIgnoreCase("No")) break;
+
+            //Get Location Data
+
+        }while (!city.equalsIgnoreCase("No"));
     }
 }
